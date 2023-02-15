4bb4eb1badbb6288cad9dd3506c5b5a520a4c2de
Divya Gupta <divygupta0319@gmail.com>
2023-02-15 23:17:52 +0530

    Injecting literal values using properties file

--- src/com/luv2code/springdemo/TrackCoach.java
+++ src/com/luv2code/springdemo/TrackCoach.java
@@ -4,6 +4,32 @@ public class TrackCoach implements Coach {
 	
 	private FortuneService fortuneService;
 	
+	// add new fields for email address and team
+	private String emailAddress;
+	private String team;
+	
+
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+
+	public void setEmailAddress(String emailAddress) {
+		System.out.println("TrackCoach: inside setter method - setEmailAddress");
+		this.emailAddress = emailAddress;
+	}
+
+	public String getTeam() {
+		return team;
+	}
+
+
+	public void setTeam(String team) {
+		System.out.println("TrackCoach: inside setter method - setTeam");
+		this.team = team;
+	}
+
+
 	// create a no-arg constructor
 	public TrackCoach() {
 		System.out.println("TrackCoach: inside no-arg constructor");
@@ -12,7 +38,7 @@ public class TrackCoach implements Coach {
 
 	// our setter method
 	public void setFortuneService(FortuneService fortuneService) {
-		System.out.println("TrackCoach: inside setter method: setFortuneService");
+		System.out.println("TrackCoach: inside setter method - setFortuneService");
 		this.fortuneService = fortuneService;
 	}
 
