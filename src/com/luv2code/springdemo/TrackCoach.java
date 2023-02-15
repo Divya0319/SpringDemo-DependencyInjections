439c2e9cc9c046fe229bd2ad607a5e05c921a178
Divya Gupta <divygupta0319@gmail.com>
2023-02-15 22:21:49 +0530

    Depedency injection using setter injection, using xml config file changes

--- src/com/luv2code/springdemo/TrackCoach.java
+++ src/com/luv2code/springdemo/TrackCoach.java
@@ -1,16 +1,30 @@
 package com.luv2code.springdemo;
 
 public class TrackCoach implements Coach {
+	
+	private FortuneService fortuneService;
+	
+	// create a no-arg constructor
+	public TrackCoach() {
+		System.out.println("TrackCoach: inside no-arg constructor");
+	}
+
+
+	// our setter method
+	public void setFortuneService(FortuneService fortuneService) {
+		System.out.println("TrackCoach: inside setter method: setFortuneService");
+		this.fortuneService = fortuneService;
+	}
+
 
 	@Override
 	public String getDailyWorkout() {
-		return "Run a hard 5k";
+		return "Run a hard 10k";
 	}
 
 	@Override
 	public String getDailyFortune() {
-		// TODO Auto-generated method stub
-		return null;
+		return fortuneService.getFortune();
 	}
 
 }
